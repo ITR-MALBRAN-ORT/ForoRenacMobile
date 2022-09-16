4a4a1d5a9ce3afa87da0fbb28acddbcb7f832ec0
Eliseo Carhuanca <Eliseo_Carhuanca@intranet.itresources>
2022-09-16 17:27:43 -0300

    drawer Content and styles added

---
+++ ForoRenac/app/components/customButton/customButtonSecondary.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Pressable,
+  Text,
+  View,
+  StyleSheet,
+} from 'react-native';
+
+export default CustomButtonSecondary = ({text, onPress, disabled}) => {
+  return (
+    <View style={styles.container}>
+      <Pressable style={disabled? styles.buttonDisabled : styles.button} onPress={onPress} disabled = {disabled}>
+        <Text style={styles.text}>{text}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom:10,
+    marginTop:20
+  },
+  button: {
+    minWidth: '97%',
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 22,
+    backgroundColor: 'transparent',
+    borderColor: 'white',
+    borderStyle:'solid',
+    borderWidth: 1,
+    paddingVertical: 3,
+    paddingHorizontal: 10
+  },
+  buttonDisabled:{
+    width: 192,
+    height: 33,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 22,
+    backgroundColor: '#EBEBE4',
+  },
+  text: {
+    fontSize: 14,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
