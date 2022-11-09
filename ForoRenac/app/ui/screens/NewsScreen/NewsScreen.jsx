63cf2226c4d0e1113d8bc4dbcd2da2776a0bf256
Juan ignacio Lopez <lopezjuanignacio04@gmail.com>
2022-11-09 20:41:50 -0300

    feat: news screen started

--- ForoRenac/app/ui/screens/NewsScreen/NewsScreen.jsx
+++ ForoRenac/app/ui/screens/NewsScreen/NewsScreen.jsx
@@ -1,7 +1,12 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import NewsUI from './NewsUI'
+import Data from "./data"
 
 export default function NewsScreen() {
-  return <NewsUI />
+  Data.sort((a, b) => {
+    fa = new Date(a.Fecha);
+    fb = new Date(b.Fecha);
+    return fa.getTime() - fb.getTime();
+  })
+  return <NewsUI Data={Data} />
 }
--- ForoRenac/app/ui/screens/NewsScreen/NewsUI.jsx
+++ ForoRenac/app/ui/screens/NewsScreen/NewsUI.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import React from 'react'
 
-export default function NewsUI() {
+export default function NewsUI({Data}) {
+  
   return (
-    <View>
-      <Text>NewsUI</Text>
+    <View style={styles.container}>
+      <Text style={styles.title} >Novedades</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10
+  },
+  title: {
+    fontSize: 24,
+    color: '#1E1E1E',
+    fontWeight: '600',
+  }
+})
