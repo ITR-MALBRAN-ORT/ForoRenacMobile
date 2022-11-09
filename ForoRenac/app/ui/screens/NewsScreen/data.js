9f5e6b7fdfd0eedd3e44c341e92c3794a305d00f
Juan ignacio Lopez <lopezjuanignacio04@gmail.com>
2022-11-09 20:23:27 -0300

    feat: mock dara for news

---
+++ ForoRenac/app/ui/screens/NewsScreen/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    Titulo: 'Nombre noticia',
+    Contenido: 'Ullamco in Lorem dolor tempor id pariatur. Eu velit ullamco veniam nulla occaecat ad qui amet enim ut deserunt sit. Veniam ut id ullamco Lorem dolor adipisicing et esse incididunt eiusmod culpa. Dolore dolor anim excepteur qui officia cillum occaecat duis aliqua consectetur enim dolor in.',
+    Fecha: "2022-10-10",
+    Imagen: "https://unahur.edu.ar/wp-content/uploads/2021/03/articles_lab4x.jpg"
+  },
+  {
+    Titulo: 'Nombre noticia',
+    Contenido: 'Ullamco in Lorem dolor tempor id pariatur. Eu velit ullamco veniam nulla occaecat ad qui amet enim ut deserunt sit. Veniam ut id ullamco Lorem dolor adipisicing et esse incididunt eiusmod culpa. Dolore dolor anim excepteur qui officia cillum occaecat duis aliqua consectetur enim dolor in.',
+    Fecha: "2022-10-9",
+    Imagen: null
+  },
+  {
+    Titulo: 'Nombre noticia',
+    Contenido: 'Ullamco in Lorem dolor tempor id pariatur. Eu velit ullamco veniam nulla occaecat ad qui amet enim ut deserunt sit. Veniam ut id ullamco Lorem dolor adipisicing et esse incididunt eiusmod culpa. Dolore dolor anim excepteur qui officia cillum occaecat duis aliqua consectetur enim dolor in.',
+    Fecha: "2022-10-9",
+    Imagen: null
+  },
+  {
+    Titulo: 'Nombre noticia',
+    Contenido: 'Ullamco in Lorem dolor tempor id pariatur. Eu velit ullamco veniam nulla occaecat ad qui amet enim ut deserunt sit. Veniam ut id ullamco Lorem dolor adipisicing et esse incididunt eiusmod culpa. Dolore dolor anim excepteur qui officia cillum occaecat duis aliqua consectetur enim dolor in.',
+    Fecha: "2022-10-9",
+    Imagen: null
+  },
+  {
+    Titulo: 'Nombre noticia',
+    Contenido: 'Ullamco in Lorem dolor tempor id pariatur. Eu velit ullamco veniam nulla occaecat ad qui amet enim ut deserunt sit. Veniam ut id ullamco Lorem dolor adipisicing et esse incididunt eiusmod culpa. Dolore dolor anim excepteur qui officia cillum occaecat duis aliqua consectetur enim dolor in.',
+    Fecha: "2022-10-9",
+    Imagen: null
+  },
+];
